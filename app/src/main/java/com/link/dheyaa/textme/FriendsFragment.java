14e5866745a5203026724b90479e9e00a5bb143b
Dheyaa <alialiali3957@gmail.com>
2018-12-10 21:37:25 +0200

    new commit for our project

--- app/src/main/java/com/link/dheyaa/textme/FriendsFragment.java
+++ app/src/main/java/com/link/dheyaa/textme/FriendsFragment.java
@@ -3,26 +3,45 @@ package com.link.dheyaa.textme;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 
 public class FriendsFragment extends android.support.v4.app.Fragment {
 
     @Nullable
-    @Override
+    ListView listView ;
+    private FirebaseAuth mAuth;
+    private DatabaseReference DBref;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.friends_tab, container, false);
-        ListView listView = (ListView) root.findViewById(R.id.friends_list);
+         listView = (ListView) root.findViewById(R.id.friends_list);
+
+        mAuth = FirebaseAuth.getInstance();
+        DBref = FirebaseDatabase.getInstance().getReference("Users");
 
 
-        ArrayList<Friend> friends = new ArrayList<>();
+        final ArrayList<Friend> friends = new ArrayList<>();
+
+     //   DBref.child(mAuth.getCurrentUser().getUid()).addValueEventListener(userEventListener);
+
 
         friends.add(new Friend("peter" , "peter@peter.peter"));
         friends.add(new Friend("malcolm" , "malcolm@malcolm.malcolm"));
@@ -35,12 +54,35 @@ public class FriendsFragment extends android.support.v4.app.Fragment {
        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
            @Override
            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+               listView.setClickable(false);
                Intent Message = new Intent(getActivity(), MessagingPage.class);
+               Message.putExtra("Friend_name", friends.get(i).getName());
                startActivity(Message);
+               listView.setClickable(true);
+
            }
        });
         return root;
      }
+
+   /*
+   *
+   *  ValueEventListener userEventListener = new  ValueEventListener() {
+        @Override
+        public void onDataChange(DataSnapshot dataSnapshot) {
+            User user =  dataSnapshot.getValue(User.class);
+            Collection<String> friendsIds =  user.getFriends().values();
+            //friendsIds
+            DBref.
+
+
+        }
+        @Override
+        public void onCancelled(DatabaseError error) {
+        }
+    };
+   *
+   * */
 
 
 }
